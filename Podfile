ee8e71f20d47c53a20d4a0fae81f5b14dd680866
Key Hui <>
2020-07-18 17:31:56 +0800

    initial project

---
+++ Podfile
@@ -0,0 +1,7 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '12.0'
+
+target 'TradingChartView' do
+	use_frameworks!
+	pod 'LightweightCharts', '~> 2.0.0'
+end
---
+++ TradingChartView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TradingChartView
+//
+//  Created by Key Hui on 7/18/20.
+//  Copyright © 2020 KeyFun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
